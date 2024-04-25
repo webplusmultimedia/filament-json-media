de1d1b9b6d014e4524eac689e8572f439ec5ae4a
webplusm <contact@webplusm.net>
2024-04-24 20:55:15 -0400

    add show documents on columns (refs : columns(x))

--- resources/views/infolist/document.blade.php
+++ resources/views/infolist/document.blade.php
@@ -5,18 +5,28 @@
 @endphp
 <div x-data="{}"
      x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref("gallery-json-media-styles","webplusm/gallery-json-media"))]"
-     class="max-w-max"
 >
     @if(($record) and $record->hasDocuments($getName()))
-        <div class="grid grid-rows-1 lg:grid-cols-2 xl:grid-cols-3 gap-3 overflow-hidden">
-            @foreach(collect($record->getDocuments($getName()))->take($getMaxAvatars())->all() as $document)
-                 <a href="{{ $document->getUrl() }}" target="_blank"
-                    class="inline-flex items-center gap-x-1 text-xs text-primary-600 hover:text-primary-500"
-                 >
-                 @svg('heroicon-o-document','h-6 w-6')    {{ $document->getCustomProperty('alt') }}
-                 </a>
+        <x-filament::grid
+            :default="$getColumns('default')"
+            :sm="$getColumns('sm')"
+            :md="$getColumns('md')"
+            :lg="$getColumns('lg')"
+            :xl="$getColumns('xl')"
+            :two-xl="$getColumns('2xl')"
+            :attributes="
+            \Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())
+                ->class(['fi-fo-component-ctn gap-6'])
+        "
+        >
+            @foreach($record->getDocuments($getName()) as $document)
+                <a href="{{ $document->getUrl() }}" target="_blank"
+                   class="inline-flex items-center gap-x-1 text-xs text-primary-600 hover:text-primary-500"
+                >
+                    @svg('heroicon-o-document','h-6 w-6')    {{ $document->getCustomProperty('alt') }}
+                </a>
             @endforeach
-        </div>
+        </x-filament::grid>
     @else
         <div>
             {{ __('gallery-json-media::infolist.document.nothing-to-show') }}
