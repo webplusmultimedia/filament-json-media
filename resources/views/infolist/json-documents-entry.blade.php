6c9dc44cc6eb7bee9290ab81abfb92dd507586a1
WebplusM <contact@webplusm.net>
2025-07-02 18:51:12 -0400

    wip

--- resources/views/infolist/json-documents-entry.blade.php
+++ resources/views/infolist/json-documents-entry.blade.php
@@ -1,6 +1,3 @@
 <x-filament-infolists::entry-wrapper >
-    <x-filament-infolists::entry-wrapper.label >
-        {{ $getLabel() }}
-    </x-filament-infolists::entry-wrapper.label>
     @include('gallery-json-media::infolist.document')
 </x-filament-infolists::entry-wrapper>
