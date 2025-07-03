0aa68ae85946377c7974cdc180be1b3049539545
WebplusM <contact@webplusm.net>
2025-07-03 12:25:44 -0400

    chore: add tooltip translations and attributes for media actions

--- resources/lang/fr/gallery-json-media.php
+++ resources/lang/fr/gallery-json-media.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 // translations for GalleryJsonMedia/FilamentJsonMedia
 return [
     'Drag&Drop' => 'Drag & Drop ou cliquer ICI',
-    'tooltip.edit-button-custom-property' => 'Edition des propriétés personnalisées',
+    'tooltip.edit-button-custom-property' => 'Editer les propriétés personnalisées',
+    'tooltip.delete-media-button' => 'Supprimer le média',
     'title.edit-modal-form-for-customs-properties' => 'Edition des propriétés personnalisées',
     'infoList.document.nothing-to-show' => 'Pas de document',
     'form.alt.label.media' => 'Alt',
