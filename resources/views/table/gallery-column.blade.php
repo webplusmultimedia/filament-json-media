9a44161036dfe2c1c6cd4a51ae23c1050ec839b3
WebplusM <contact@webplusm.net>
2025-08-17 20:31:27 -0400

    fix: entry json media

--- resources/views/table/gallery-column.blade.php
+++ resources/views/table/gallery-column.blade.php
@@ -1,26 +1,26 @@
 @php
     use GalleryJsonMedia\JsonMedia\Contracts\HasMedia;
         /** @var HasMedia $record */
-        $record = $getRecord();
+
 @endphp
-<div class="flex gap-x-1 items-center max-w-max">
+<div class="flex gap-x-1 items-center -space-x-5 p-2 overflow-x-auto overflow-y-hidden" style="height: {{ $getThumbHeight() + 16 }}px">
     @if(($record) and !$record->hasDocuments($getName()))
         @if(!$hasAvatars())
             {{ $record->getFirstMedia($getName())?->withImageProperties($getThumbWidth(),$getThumbHeight()) }}
         @else
-            <div class="flex  -space-x-5 p-2 overflow-hidden" style="max-height: {{ $getThumbHeight() + 16 }}px">
-                @foreach(collect($record->getMedias($getName()))->take($getMaxAvatars())->all() as $media)
-                    <img class="inline-block rounded-full ring-2 ring-white object-cover" src="{{ $media->getCropUrl($getThumbWidth(),$getThumbHeight()) }}"
-                         alt="{{ $media->getCustomProperty('alt') }}" width="{{ $getThumbWidth() }}"
-                         height="{{ $getThumbHeight() }}"
-                         loading="lazy"
-                    />
-                @endforeach
-            </div>
+            @foreach(collect($record->getMedias($getName()))->take($getMaxAvatars())->all() as $media)
+                <img  class="wm-image ring-white dark:ring-gray-900 inline-block rounded-full object-cover {{ $getRing() }}"
+                      style="height: {{ $getThumbHeight() }}px;width: {{ $getThumbHeight() }}px"
+                      src="{{ $media->getCropUrl($getThumbWidth(),$getThumbHeight()) }}"
+                      alt="{{ $media->getCustomProperty('alt') }}"
+                      width="{{ $getThumbWidth() }}"
+                      height="{{ $getThumbHeight() }}"
+                      loading="lazy"
+                />
+            @endforeach
             @if(($nb = ($record->mediasCount($getName()) - collect($record->getMedias($getName()))->take($getMaxAvatars())->count())) > 0)
-                <x-filament::badge color="info" size="xs">+ {{ $nb }}</x-filament::badge>
+                    <x-filament::badge color="info" size="xs">+ {{ $nb }}</x-filament::badge>
             @endif
-
         @endif
     @endif
 </div>
--- src/Support/Concerns/HasThumbProperties.php
+++ src/Support/Concerns/HasThumbProperties.php
@@ -45,4 +45,29 @@ trait HasThumbProperties
     {
         return $this->evaluate($this->thumbHeight);
     }
+
+    protected ?int $ring = null;
+
+    /** @param int<1,4> $ring */
+    public function ring(int $ring): self
+    {
+        $this->ring = $ring;
+
+        return $this;
+    }
+
+    public function getRing(): string
+    {
+        if ($this->ring === null) {
+            $this->ring = 2; // Default ring value
+        }
+
+        return match ($this->ring) {
+            1 => 'ring-1',
+            2 => 'ring-2',
+            3 => 'ring-3',
+            4 => 'ring-4',
+            default => 'ring-2', // Fallback to medium ring
+        };
+    }
 }
