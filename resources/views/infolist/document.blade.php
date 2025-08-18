d6a12d84071f3b69cfcf98ec140d9176cf475ebf
WebplusM <contact@webplusm.net>
2025-08-18 18:40:51 -0400

    fix: update document grid layout for better responsiveness

--- resources/views/infolist/document.blade.php
+++ resources/views/infolist/document.blade.php
@@ -10,13 +10,8 @@
 >
     @if(($record) and $record->hasDocuments($getName()))
         <div
-            {{
-                $attributes->class([
-                    "grid grid-cols-4 gap-4",
-                ])
-            }}
+            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
         >
-
             @foreach($record->getDocuments($getName()) as $document)
                 <a href="{{ $document->getUrl() }}" target="_blank"
                    class="inline-flex items-center gap-x-1 text-xs text-primary-600 hover:text-primary-500"
